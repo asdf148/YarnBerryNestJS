d2f51e2610053432709e98abcef2209f4b3ee7ce
asdf148 <wnduf0405_@naver.com>
2022-02-08 22:14:54 +0900

    :test_tube: :: Write the SignUp success test code
    Write the SignUp success test code

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -1,18 +1,40 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { hash } from 'bcrypt';
 import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { Auth } from './entity/auth.entity';
 
 describe('AuthController', () => {
   let controller: AuthController;
+  let service: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      controllers: [AuthController],
+      controllers: [AuthController, AuthService],
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
+    service = module.get<AuthService>(AuthService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('회원가입 성공', async () => {
+    const foundAuth: Auth = new Auth(
+      null,
+      null,
+      'asdf',
+      'asdf@asdf.com',
+      await hash('asdfqw12', 10),
+      [],
+    );
+
+    jest
+      .spyOn(service, 'signUp')
+      .mockImplementation(() => Promise.resolve(foundAuth));
+
+    expect(await controller.signUp(foundAuth)).toBe(foundAuth);
   });
 });
