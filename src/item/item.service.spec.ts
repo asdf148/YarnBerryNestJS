f9bab46a792106bac78a49dfdd61d103989cb118
asdf148 <wnduf0405_@naver.com>
2022-02-13 11:32:16 +0900

    [MODIFY] Modify Test code

--- src/item/item.service.spec.ts
+++ src/item/item.service.spec.ts
@@ -1,16 +1,29 @@
+import { getModelToken } from '@nestjs/mongoose';
 import { Test, TestingModule } from '@nestjs/testing';
 import { CreateItem } from './dto/createItem.dto';
+import { Item } from './entity/item.entity';
+import { ItemRepository } from './entity/item.repository';
 import { ItemService } from './item.service';
 
 describe('ItemService', () => {
   let service: ItemService;
+  let repository: ItemRepository;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ItemService],
+      providers: [
+        ItemService,
+        ItemRepository,
+        {
+          provide: getModelToken(Item.name),
+          // eslint-disable-next-line @typescript-eslint/no-empty-function
+          useFactory: () => {},
+        },
+      ],
     }).compile();
 
     service = module.get<ItemService>(ItemService);
+    repository = module.get<ItemRepository>(ItemRepository);
   });
 
   it('should be defined', () => {
@@ -30,7 +43,21 @@ describe('ItemService', () => {
       },
     );
 
-    const result: string = await service.createItem(createItem);
+    const savedItem = new Item(
+      null,
+      createItem.location,
+      createItem.title,
+      createItem.star,
+      createItem.content,
+      createItem.category,
+      createItem.writer,
+    );
+
+    jest
+      .spyOn(repository, 'save')
+      .mockImplementation(() => Promise.resolve(savedItem));
+
+    const result: string = await service.createItem(null, createItem);
     expect(result).toBe('Create Item Success');
   });
 });
